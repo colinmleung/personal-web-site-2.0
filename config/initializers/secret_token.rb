dcbc546e10a88b7f0a4afff1340d9af06a7f20b3
Colin Leung <colinmleung@gmail.com>
2013-09-14 21:19:35 -0700

    Changed Gemfiles and secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,27 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PersonalWebsite::Application.config.secret_token = '9196879b80cda4f6eb334199cd49b49fbd780a179e1ae3f19c724a1ccb6fffa3dff10589ffc61db16a8c83e72f0fefd9424173af07eaa681272352715ced5174'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+PersonalWebsite::Application.config.secret_key_base = secure_token
